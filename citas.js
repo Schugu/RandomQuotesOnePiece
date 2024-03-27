db2df91f3c0b99d9dcd492be197ea0f58617afde
Schugu <47997052+Schugu@users.noreply.github.com>
2024-03-27 18:22:20 -0300

    Agregue mas citas textuales

--- citas.js
+++ citas.js
@@ -22,5 +22,33 @@ const citas = [
     {
         nombre: '-Fujitora',
         cita: '"Si se preocuparan por algo tan trivial como perder el respeto, entonces es mejor no tenerlo en primer lugar."'
-    }
+    },
+    {
+        nombre: '-Nico Robin',
+        cita: '"Los tontos que no respetan el pasado, con seguridad lo repetirán."'
+    },
+    {
+        nombre: '-Brook',
+        cita: '"Ni Dios ni el diablo pueden ayudar a aquellos que no tienen deseos de pelear."'
+    },
+    {
+        nombre: '-Brook',
+        cita: '"No importa qué tan oscura sea la noche, siempre pasa a ser de día."'
+    },
+    {
+        nombre: '-Donquixote Rosinante',
+        cita: '"Moriré riendo, porque si alguna vez piensas en mí en el futuro, quiero que me recuerdes con una sonrisa."'
+    },
+    {
+        nombre: '-Jimbe',
+        cita: '"Puede ser difícil ahora, ¡pero debes silenciar esos pensamientos! ¡Deja de contar solamente las cosas que has perdido! Lo que ya no está, ¡no está! Pregúntate esto mejor: ¿Qué es lo que todavía te queda?."'
+    },
+    {
+        nombre: '-Monkey D. Luffy',
+        cita: '"No importa qué tan difícil o imposible sea, no le pierdas la vista a tu objetivo."'
+    },
+    {
+        nombre: '-Dr. Hiluluk',
+        cita: '"¿Cuándo crees que la gente muere? ¿Cuando les dan un tiro con una pistola? No. ¿Cuando les afecta una enfermedad incurable? No. ¿Cuando toman una sopa de un hongo venenoso? ¡No! Mueren cuando son olvidados."'
+    }   
 ];
